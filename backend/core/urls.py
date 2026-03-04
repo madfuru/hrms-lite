058a00b054b6f13f0a52bbafd944f5847fe3cd84
madfuru <100013547+madfuru@users.noreply.github.com>
2026-03-04 08:30:15 +0530

    Update urls.py

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import EmployeeViewSet, AttendanceViewSet, dashboard_summary, today_attendance
 
-routerer = DefaultRouter()
+router = DefaultRouter()
 router.register(r'employees', EmployeeViewSet)
 router.register(r'attendance', AttendanceViewSet)
 
